c350a761f5f6f6eb2bddc40448e7d1840abfc29b
imhele <work@imhele.com>
2019-05-16 21:17:49 +0800

    temporarily save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native~</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+        <Text style={styles.instructions}>To get started, edit src/pages/index.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
