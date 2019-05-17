61c76ba5eaa4c57aa66f222b193a769ac9693b2e
imhele <work@imhele.com>
2019-05-18 00:53:19 +0800

    rename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,11 @@ import Provider from '@ant-design/react-native/es/provider';
 import { ReloadContainer } from '@/components/reload';
 import { WrappedContainer } from '@/layouts/Routes';
 import React from 'react';
-import { Text } from 'react-native';
 
 export default () => (
   <ReloadContainer>
     <Provider>
-      <Text>HHH</Text>
+      <WrappedContainer />
     </Provider>
   </ReloadContainer>
 );
